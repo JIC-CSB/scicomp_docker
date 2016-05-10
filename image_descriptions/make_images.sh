5fa3f1261f80ba4d0a10a08bdef5d7775ea64453
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-05-10 13:07:25 +0100

    Added some logging to build script

--- image_descriptions/make_images.sh
+++ image_descriptions/make_images.sh
@@ -3,6 +3,7 @@
 IMAGES=(centos_python_pip scientific_python image_python bioformats)
 
 for IMAGE in ${IMAGES[@]}; do
+    echo "Building jicscicomp/$IMAGE"
     cd $IMAGE
     docker build -t jicscicomp/$IMAGE .
     cd ..
